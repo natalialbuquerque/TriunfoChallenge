dcbf6493891f4b3ff895eabaf2dd37d6457e31e8
Natália Pessoa de Azevedo Albuquerque <npaa@192.168.1.12>
2022-07-05 08:40:29 -0300

    já com o gitingnore

---
+++ TriunfoChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriunfoChallenge
+//
+//  Created by Natália Pessoa de Azevedo Albuquerque on 04/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
